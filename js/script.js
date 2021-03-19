4a8ec313493e0ed4fe5d94edca8713b6e22780ef
valerio sgura <valeriosgura88@gmail.com>
2021-03-19 18:13:26 +0100

    more solutions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,60 @@
-function showHamb() {
-    $('.hamburger-menu').show();
+function sol1() {
+
+    function showHamb() {
+        $('.hamburger-menu').show();
+    }
+
+    function hideHamb() {
+        $('.hamburger-menu').hide();
+    }
+
+    function init() {
+        $('.fas.fa-bars').click(function () {
+            showHamb();
+        });
+
+        $('.fas.fa-times').click(function () {
+            hideHamb();
+        });
+    }
+
+    init();
 }
 
-function hideHamb() {
-    $('.hamburger-menu').hide();
+function sol2() {
+
+    function showHamb() {
+        $('.hamburger-menu').addClass('active');
+    }
+
+    function hideHamb() {
+        $('hamburger-menu').removeClass('active');
+    }
+
+    function init() {
+        $('.header-right > a').click(showHamb);
+        $('.hamburger-menu > a').click(hideHamb);
+    }
+
+    init();
 }
 
-function init() {
-    $('.fas.fa-bars').click(function() {
-        showHamb();
-    });
+function sol3 () {
 
-    $('.fas.fa-times').click(function() {
-        hideHamb();
-    });
+    function hambToggle() {
+        $('.hamburger-menu').toggleClass('active');
+    }
+
+    function init() {
+        $('.header-right > a, .hamburger-menu > a').click(hambToggle);
+    }
+
+    init();
 }
 
-init();
+//sol1();
+
+//sol2();
+
+//sol3();
+
